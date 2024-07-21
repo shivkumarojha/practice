dbcec3d330801c1f07ecf6a40a83cf169a882653
shivkumarojha <ojhashivkumar88@gmail.com>
2024-07-21 17:33:44 +0530

    Week 2 practice done

--- week2/index.ts
+++ week2/index.ts
@@ -1,9 +1,21 @@
+import 'dotenv/config'
 import express from "express"
+
 const app = express()
+
+// parse json data
 app.use(express.json())
+
+// Parse form data
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/api", (req, res) => {
+    const header1 = req.headers['user-agent']
     res.json({
-        message: "This is api endpoint"
+        message: "This is api endpoint",
+        header1: header1,
+        language: req.headers['accept-language'],
+        ip: req.ip
     })
 })
 app.get("/health", (req, res) => {
@@ -26,6 +38,9 @@ app.post("/submit-data", (req, res) => {
         allTextContent: textContent
     })
 })
-app.listen(3000, () => {
-    console.log("server is running at port ")
+
+// encode form data
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running at port ${process.env.PORT}`)
 })
